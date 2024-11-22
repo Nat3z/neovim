2d8d923f181d35c8c90b6eb17d7bffa34f47d400
Nat3z <66748576+Nat3z@users.noreply.github.com>
2024-11-21 20:33:22 -0800

    Tmux

--- lua/plugins/tmux-navigator.lua
+++ lua/plugins/tmux-navigator.lua
@@ -1,3 +1,23 @@
+-- implement this vim coded
+-- function! s:set_is_vim()
+--   call s:TmuxCommand("set-option -p @is_vim yes")
+-- endfunction
+--
+-- function! s:unset_is_vim()
+--   call s:TmuxCommand("set-option -p -u @is_vim")
+-- endfunction
+--
+-- augroup tmux_navigator_is_vim
+--   au!
+--   autocmd VimEnter * call s:set_is_vim()
+--   autocmd VimLeave * call s:unset_is_vim()
+--   if exists('##VimSuspend')
+--     autocmd VimSuspend * call s:unset_is_vim()
+--     autocmd VimResume * call s:set_is_vim()
+--   endif
+-- augroup END
+--
+
 return {
   'christoomey/vim-tmux-navigator',
   event = 'VeryLazy',
@@ -8,4 +28,19 @@ return {
     'TmuxNavigateRight',
     'TmuxNavigatePrevious',
   },
+
+  -- now add the keybinds for tmux for the augroup above
+  config = function()
+    vim.cmd [[
+      augroup tmux_navigator_is_vim
+      au!
+      autocmd VimEnter * lua vim.fn.system 'tmux set-option -p @is_vim yes'
+      autocmd VimLeave * lua vim.fn.system 'tmux set-option -p -u @is_vim'
+      if exists('##VimSuspend')
+        autocmd VimSuspend * lua vim.fn.system 'tmux set-option -p -u @is_vim'
+        autocmd VimResume * vim.fn.system 'tmux set-option -p @is_vim yes'
+      endif
+      augroup END
+    ]]
+  end,
 }
