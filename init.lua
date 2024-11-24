e7dcb8ce48ead9248186e25c40fe0c856bf942e1
Nat3z <66748576+Nat3z@users.noreply.github.com>
2024-11-23 20:15:05 -0800

    Fixed TMUX (again)

--- init.lua
+++ init.lua
@@ -90,7 +90,11 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-require 'keymaps'
+
+vim.keymap.set('n', '<leader>h', '<cmd>lua os.execute "tmux select-pane -L"<cr>', { silent = false })
+vim.keymap.set('n', '<leader>j', '<cmd>lua os.execute "tmux select-pane -D"<cr>', { silent = false })
+vim.keymap.set('n', '<leader>k', '<cmd>lua os.execute "tmux select-pane -U"<cr>', { silent = false })
+vim.keymap.set('n', '<leader>l', '<cmd>lua os.execute "tmux select-pane -R"<cr>', { silent = false })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,1 @@
-vim.keymap.set('n', '<C-h>', '<cmd>TmuxNavigateLeft<cr>', { silent = true })
-vim.keymap.set('n', '<C-j>', '<cmd>TmuxNavigateDown<cr>', { silent = true })
-vim.keymap.set('n', '<C-k>', '<cmd>TmuxNavigateUp<cr>', { silent = true })
-vim.keymap.set('n', '<C-l>', '<cmd>TmuxNavigateRight<cr>', { silent = true })
+
