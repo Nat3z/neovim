5c8b5f7ca16137863ea0e803f47b2ce7568b26a7
Nat3z <66748576+Nat3z@users.noreply.github.com>
2026-02-11 20:18:36 -0800

    feat: new copilot

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -136,7 +136,6 @@ return { -- Autocompletion
           -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
           group_index = 0,
         },
-        { name = 'copilot', group_index = 2 },
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'path' },
