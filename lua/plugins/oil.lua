556dfde2cca13ac0217746a58ebdb99c8cff3119
Nat3z <66748576+Nat3z@users.noreply.github.com>
2026-02-15 20:19:06 -0800

    feat: open oil by default

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,17 +1,28 @@
 return {
   'stevearc/oil.nvim',
+  lazy = false,
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  -- dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
   init = function()
-    require('oil').setup {
-      view_options = {
-        show_hidden = true,
-      },
-    }
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+    vim.api.nvim_create_autocmd('VimEnter', {
+      once = true,
+      callback = function()
+        if vim.fn.argc() == 0 then
+          vim.schedule(function()
+            require('oil').open()
+          end)
+        end
+      end,
+    })
   end,
 }
